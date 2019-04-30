2e6eefb191b1baef2532bc70df002885cfbc14bc
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-04-30 09:02:30 -0700

    Add Travis CI to verify commands work correctly

---
+++ .travis/bundle-and-build.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# Ensure all Spago bundle and parcel build commands actually work.
+# Rather than using `parcel serve`, we'll use `parcel build`.
+
+# Static HTML
+spago bundle -m StaticHTML.StaticHTML -t assets/static-html/static-html.js
+STATIC_HTML_BUNDLE=$?
+parcel build assets/static-html/static-html.html -o static-html--parcelified.html
+STATIC_HTML_BUILD=$?
+
+# Adding properties
+spago bundle -m StaticHTML.AddingProperties -t assets/static-html/adding-properties.js
+ADDING_PROPERTIES_BUNDLE=$?
+parcel build assets/static-html/adding-properties.html -o adding-properties--parcelified.html
+ADDING_PROPERTIES_BUILD=$?
+
+# Adding CSS
+spago bundle -m StaticHTML.AddingCSS -t assets/static-html/adding-css.js
+ADDING_CSS_BUNDLE=$?
+parcel build assets/static-html/adding-css.html -o adding-css--parcelified.html
+ADDING_CSS_BUNDLE=$?
+
+# Adding state
+spago bundle -m DynamicHtml.AddingState -t assets/dynamic-html/adding-state.js
+ADDING_STATE_BUNDLE=$?
+parcel build assets/dynamic-html/adding-state.html -o adding-state--parcelified.html
+ADDING_STATE_BUNDLE=$?
+
+# Adding event handling
+spago bundle -m DynamicHtml.AddingEventHandling -t assets/dynamic-html/adding-event-handling.js
+ADDING_EVENT_HANDLING_BUNDLE=$?
+parcel build assets/dynamic-html/adding-event-handling.html -o adding-event-handling--parcelified.html
+ADDING_EVENT_HANDLING_BUNDLE=$?
+
+# Input only
+spago bundle -m ParentChildRelationships.ChildlikeComponents.InputOnly -t assets/parent-child-relationships/childlike-components/input-only.js
+INPUT_ONLY_BUNDLE=$?
+parcel build assets/parent-child-relationships/childlike-components/input-only.html -o input-only--parcelified.html
+INPUT_ONLY_BUNDLE=$?
+
+# Message only
+spago bundle -m ParentChildRelationships.ChildlikeComponents.MessageOnly -t assets/parent-child-relationships/childlike-components/message-only.js
+MESSAGE_ONLY_BUNDLE=$?
+parcel build assets/parent-child-relationships/childlike-components/message-only.html -o message-only--parcelified.html
+MESSAGE_ONLY_BUNDLE=$?
+
+# Input and Message
+spago bundle -m ParentChildRelationships.ChildlikeComponents.InputMessage -t assets/parent-child-relationships/childlike-components/input-message.js
+INPUT_MESSAGE_BUNDLE=$?
+parcel build assets/parent-child-relationships/childlike-components/input-message.html -o input-message--parcelified.html
+INPUT_MESSAGE_BUNDLE=$?
+
+####
+echo "$STATIC_HTML_BUNDLE - Static HTML - Bundle"
+echo "$STATIC_HTML_BUILD - Static HTML - Build"
+echo "$ADDING_PROPERTIES_BUNDLE - Adding Properties - Bundle"
+echo "$ADDING_PROPERTIES_BUILD - Adding Properties - Build"
+echo "$ADDING_CSS_BUNDLE - Adding CSS - Bundle"
+echo "$ADDING_CSS_BUNDLE - Adding CSS - Build"
+echo "$ADDING_STATE_BUNDLE - Adding State - Bundle"
+echo "$ADDING_STATE_BUNDLE - Adding State - Build"
+echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Bundle"
+echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Build"
+echo "$INPUT_ONLY_BUNDLE - Input only - Bundle"
+echo "$INPUT_ONLY_BUNDLE - Input only - Build"
+echo "$MESSAGE_ONLY_BUNDLE - Message only - Bundle"
+echo "$MESSAGE_ONLY_BUNDLE - Message only - Build"
+echo "$INPUT_MESSAGE_BUNDLE - Input Message - Bundle"
+echo "$INPUT_MESSAGE_BUNDLE - Input Message - Build"
+
+if [ $STATIC_HTML_BUILD == 0 ] &&
+   [ $ADDING_PROPERTIES_BUNDLE == 0 ] &&
+   [ $ADDING_PROPERTIES_BUILD == 0 ] &&
+   [ $ADDING_CSS_BUNDLE == 0 ] &&
+   [ $ADDING_CSS_BUNDLE == 0 ] &&
+   [ $ADDING_STATE_BUNDLE == 0 ] &&
+   [ $ADDING_STATE_BUNDLE == 0 ] &&
+   [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
+   [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
+   [ $INPUT_ONLY_BUNDLE == 0 ] &&
+   [ $INPUT_ONLY_BUNDLE == 0 ] &&
+   [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
+   [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
+   [ $INPUT_MESSAGE_BUNDLE == 0 ] &&
+   [ $INPUT_MESSAGE_BUNDLE == 0 ]
+then
+  echo "Build Succeeded"
+  exit 0;
+else
+  echo "Build Failed"
+  exit 1;
+fi
---
+++ .travis/print-versions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "Versions of Installed Packages"
+echo "=============================="
+
+echo "Node:"
+node --version
+
+echo "NPM:"
+npm --version
+
+echo "Purescript:"
+purs --version
+
+echo "Spago:"
+spago version
+
+echo "Parcel:"
+parcel --version
+
+echo "=============================="
+echo ""
