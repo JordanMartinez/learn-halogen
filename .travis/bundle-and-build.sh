16ecd577cdb20689b9766605f1515035d8286167
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-05-02 19:04:52 -0700

    Fix typos: check parcel build commands

--- .travis/bundle-and-build.sh
+++ .travis/bundle-and-build.sh
@@ -19,31 +19,31 @@ ADDING_PROPERTIES_BUILD=$?
 spago bundle -m StaticHTML.AddingCSS -t assets/static-html/adding-css.js
 ADDING_CSS_BUNDLE=$?
 parcel build assets/static-html/adding-css.html -o adding-css--parcelified.html
-ADDING_CSS_BUNDLE=$?
+ADDING_CSS_BUILD=$?
 
 # Adding state
 spago bundle -m DynamicHtml.AddingState -t assets/dynamic-html/adding-state.js
 ADDING_STATE_BUNDLE=$?
 parcel build assets/dynamic-html/adding-state.html -o adding-state--parcelified.html
-ADDING_STATE_BUNDLE=$?
+ADDING_STATE_BUILD=$?
 
 # Adding event handling
 spago bundle -m DynamicHtml.AddingEventHandling -t assets/dynamic-html/adding-event-handling.js
 ADDING_EVENT_HANDLING_BUNDLE=$?
 parcel build assets/dynamic-html/adding-event-handling.html -o adding-event-handling--parcelified.html
-ADDING_EVENT_HANDLING_BUNDLE=$?
+ADDING_EVENT_HANDLING_BUILD=$?
 
 # Input only
 spago bundle -m ParentChildRelationships.ChildlikeComponents.InputOnly -t assets/parent-child-relationships/childlike-components/input-only.js
 INPUT_ONLY_BUNDLE=$?
 parcel build assets/parent-child-relationships/childlike-components/input-only.html -o input-only--parcelified.html
-INPUT_ONLY_BUNDLE=$?
+INPUT_ONLY_BUILD=$?
 
 # Message only
 spago bundle -m ParentChildRelationships.ChildlikeComponents.MessageOnly -t assets/parent-child-relationships/childlike-components/message-only.js
 MESSAGE_ONLY_BUNDLE=$?
 parcel build assets/parent-child-relationships/childlike-components/message-only.html -o message-only--parcelified.html
-MESSAGE_ONLY_BUNDLE=$?
+MESSAGE_ONLY_BUILD=$?
 
 # Input and Message
 spago bundle -m ParentChildRelationships.ChildlikeComponents.All -t assets/parent-child-relationships/childlike-components/all.js
@@ -73,15 +73,15 @@ echo "$STATIC_HTML_BUILD - Static HTML - Build"
 echo "$ADDING_PROPERTIES_BUNDLE - Adding Properties - Bundle"
 echo "$ADDING_PROPERTIES_BUILD - Adding Properties - Build"
 echo "$ADDING_CSS_BUNDLE - Adding CSS - Bundle"
-echo "$ADDING_CSS_BUNDLE - Adding CSS - Build"
+echo "$ADDING_CSS_BUILD - Adding CSS - Build"
 echo "$ADDING_STATE_BUNDLE - Adding State - Bundle"
-echo "$ADDING_STATE_BUNDLE - Adding State - Build"
+echo "$ADDING_STATE_BUILD - Adding State - Build"
 echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Bundle"
-echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Build"
+echo "$ADDING_EVENT_HANDLING_BUILD - Adding Event Handling - Build"
 echo "$INPUT_ONLY_BUNDLE - Input only - Bundle"
-echo "$INPUT_ONLY_BUNDLE - Input only - Build"
+echo "$INPUT_ONLY_BUILD - Input only - Build"
 echo "$MESSAGE_ONLY_BUNDLE - Message only - Bundle"
-echo "$MESSAGE_ONLY_BUNDLE - Message only - Build"
+echo "$MESSAGE_ONLY_BUILD - Message only - Build"
 echo "$ALL_BUNDLE - Input Message - Bundle"
 echo "$ALL_BUILD - Input Message - Build"
 echo "$BASIC_CONTAINER_BUNDLE - Basic Container - Bundle"
@@ -91,19 +91,20 @@ echo "$PARENT_INPUT_ONLY_BUILD - Parent Input Only - Build"
 echo "$PARENT_MESSAGE_ONLY_BUNDLE - Parent Message Only - Bundle"
 echo "$PARENT_MESSAGE_ONLY_BUILD - Parent Message Only - Build"
 
-if [ $STATIC_HTML_BUILD == 0 ] &&
+if [ $STATIC_HTML_BUNDLE == 0 ] &&
+   [ $STATIC_HTML_BUILD == 0 ] &&
    [ $ADDING_PROPERTIES_BUNDLE == 0 ] &&
    [ $ADDING_PROPERTIES_BUILD == 0 ] &&
    [ $ADDING_CSS_BUNDLE == 0 ] &&
-   [ $ADDING_CSS_BUNDLE == 0 ] &&
+   [ $ADDING_CSS_BUILD == 0 ] &&
    [ $ADDING_STATE_BUNDLE == 0 ] &&
-   [ $ADDING_STATE_BUNDLE == 0 ] &&
+   [ $ADDING_STATE_BUILD == 0 ] &&
    [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
    [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
    [ $INPUT_ONLY_BUNDLE == 0 ] &&
-   [ $INPUT_ONLY_BUNDLE == 0 ] &&
+   [ $INPUT_ONLY_BUILD == 0 ] &&
    [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
-   [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
+   [ $MESSAGE_ONLY_BUILD == 0 ] &&
    [ $ALL_BUNDLE == 0 ] &&
    [ $ALL_BUILD == 0 ] &&
    [ $BASIC_CONTAINER_BUNDLE == 0 ] &&
