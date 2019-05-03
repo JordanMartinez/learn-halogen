e8b18b5fc6843f61655cc3b4ae8a129957285876
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-05-03 11:30:12 -0700

    Remove text left behind when fixing merge conflicts

--- .travis/bundle-and-build.sh
+++ .travis/bundle-and-build.sh
@@ -19,47 +19,29 @@ ADDING_PROPERTIES_BUILD=$?
 spago bundle -m StaticHTML.AddingCSS -t assets/static-html/adding-css.js
 ADDING_CSS_BUNDLE=$?
 parcel build assets/static-html/adding-css.html -o adding-css--parcelified.html
-<<<<<<< HEAD
 ADDING_CSS_BUILD=$?
-=======
-ADDING_CSS_BUNDLE=$?
->>>>>>> origin/latestRelease
 
 # Adding state
 spago bundle -m DynamicHtml.AddingState -t assets/dynamic-html/adding-state.js
 ADDING_STATE_BUNDLE=$?
 parcel build assets/dynamic-html/adding-state.html -o adding-state--parcelified.html
-<<<<<<< HEAD
 ADDING_STATE_BUILD=$?
-=======
-ADDING_STATE_BUNDLE=$?
->>>>>>> origin/latestRelease
 
 # Adding event handling
 spago bundle -m DynamicHtml.AddingEventHandling -t assets/dynamic-html/adding-event-handling.js
 ADDING_EVENT_HANDLING_BUNDLE=$?
 parcel build assets/dynamic-html/adding-event-handling.html -o adding-event-handling--parcelified.html
-<<<<<<< HEAD
 ADDING_EVENT_HANDLING_BUILD=$?
-=======
-ADDING_EVENT_HANDLING_BUNDLE=$?
->>>>>>> origin/latestRelease
 
 # Input only
 spago bundle -m ParentChildRelationships.ChildlikeComponents.InputOnly -t assets/parent-child-relationships/childlike-components/input-only.js
 INPUT_ONLY_BUNDLE=$?
-<<<<<<< HEAD
 parcel build assets/parent-child-relationships/childlike-components/input-only.html -o child-input-only--parcelified.html
 INPUT_ONLY_BUILD=$?
-=======
-parcel build assets/parent-child-relationships/childlike-components/input-only.html -o input-only--parcelified.html
-INPUT_ONLY_BUNDLE=$?
->>>>>>> origin/latestRelease
 
 # Message only
 spago bundle -m ParentChildRelationships.ChildlikeComponents.MessageOnly -t assets/parent-child-relationships/childlike-components/message-only.js
 MESSAGE_ONLY_BUNDLE=$?
-<<<<<<< HEAD
 parcel build assets/parent-child-relationships/childlike-components/message-only.html -o child-message-only--parcelified.html
 MESSAGE_ONLY_BUILD=$?
 
@@ -95,16 +77,6 @@ spago bundle -m ParentChildRelationships.ParentlikeComponents.SimpleChild.Messag
 PARENT_MESSAGE_ONLY_BUNDLE=$?
 parcel build assets/parent-child-relationships/parentlike-components/simple-child/parent-message-only.html -o parent-message-only--parcelified.html
 PARENT_MESSAGE_ONLY_BUILD=$?
-=======
-parcel build assets/parent-child-relationships/childlike-components/message-only.html -o message-only--parcelified.html
-MESSAGE_ONLY_BUNDLE=$?
-
-# Input and Message
-spago bundle -m ParentChildRelationships.ChildlikeComponents.InputMessage -t assets/parent-child-relationships/childlike-components/input-message.js
-INPUT_MESSAGE_BUNDLE=$?
-parcel build assets/parent-child-relationships/childlike-components/input-message.html -o input-message--parcelified.html
-INPUT_MESSAGE_BUNDLE=$?
->>>>>>> origin/latestRelease
 
 ####
 echo "$STATIC_HTML_BUNDLE - Static HTML - Bundle"
@@ -112,7 +84,6 @@ echo "$STATIC_HTML_BUILD - Static HTML - Build"
 echo "$ADDING_PROPERTIES_BUNDLE - Adding Properties - Bundle"
 echo "$ADDING_PROPERTIES_BUILD - Adding Properties - Build"
 echo "$ADDING_CSS_BUNDLE - Adding CSS - Bundle"
-<<<<<<< HEAD
 echo "$ADDING_CSS_BUILD - Adding CSS - Build"
 echo "$ADDING_STATE_BUNDLE - Adding State - Bundle"
 echo "$ADDING_STATE_BUILD - Adding State - Build"
@@ -161,35 +132,6 @@ if [ $STATIC_HTML_BUNDLE == 0 ] &&
    [ $PARENT_INPUT_ONLY_BUILD == 0 ] &&
    [ $PARENT_MESSAGE_ONLY_BUNDLE == 0 ] &&
    [ $PARENT_MESSAGE_ONLY_BUILD == 0 ]
-=======
-echo "$ADDING_CSS_BUNDLE - Adding CSS - Build"
-echo "$ADDING_STATE_BUNDLE - Adding State - Bundle"
-echo "$ADDING_STATE_BUNDLE - Adding State - Build"
-echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Bundle"
-echo "$ADDING_EVENT_HANDLING_BUNDLE - Adding Event Handling - Build"
-echo "$INPUT_ONLY_BUNDLE - Input only - Bundle"
-echo "$INPUT_ONLY_BUNDLE - Input only - Build"
-echo "$MESSAGE_ONLY_BUNDLE - Message only - Bundle"
-echo "$MESSAGE_ONLY_BUNDLE - Message only - Build"
-echo "$INPUT_MESSAGE_BUNDLE - Input Message - Bundle"
-echo "$INPUT_MESSAGE_BUNDLE - Input Message - Build"
-
-if [ $STATIC_HTML_BUILD == 0 ] &&
-   [ $ADDING_PROPERTIES_BUNDLE == 0 ] &&
-   [ $ADDING_PROPERTIES_BUILD == 0 ] &&
-   [ $ADDING_CSS_BUNDLE == 0 ] &&
-   [ $ADDING_CSS_BUNDLE == 0 ] &&
-   [ $ADDING_STATE_BUNDLE == 0 ] &&
-   [ $ADDING_STATE_BUNDLE == 0 ] &&
-   [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
-   [ $ADDING_EVENT_HANDLING_BUNDLE == 0 ] &&
-   [ $INPUT_ONLY_BUNDLE == 0 ] &&
-   [ $INPUT_ONLY_BUNDLE == 0 ] &&
-   [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
-   [ $MESSAGE_ONLY_BUNDLE == 0 ] &&
-   [ $INPUT_MESSAGE_BUNDLE == 0 ] &&
-   [ $INPUT_MESSAGE_BUNDLE == 0 ]
->>>>>>> origin/latestRelease
 then
   echo "Build Succeeded"
   exit 0;
