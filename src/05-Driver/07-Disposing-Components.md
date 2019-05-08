41881d113ee64930b92898c5457824bf39f046eb
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-05-08 11:36:42 -0700

    Add example of disposing a component

---
+++ src/05-Driver/07-Disposing-Components.md
@@ -0,0 +1,13 @@
+# Disposing Components
+
+In this example, the top-level component will display how much time it has left before it will be disposed. Then it will be disposed.
+
+## Compiling Instructions
+
+To compile the next file and view its results in the browser, use these instructions.
+
+```bash
+spago bundle -m Driver.DisposingComponents -t assets/driver/disposing-components.js
+parcel serve assets/driver/disposing-components.html -o disposing-components--parcelified.html --open
+# After the page opens, check your browser's console to see the messages
+```
---
+++ src/05-Driver/08-Disposing-Components.purs
@@ -0,0 +1,79 @@
+module Driver.DisposingComponents where
+
+import Prelude
+
+import CSS (backgroundColor, em, lightgreen, padding)
+import Control.Monad.State (put)
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Aff (Aff, Milliseconds(..), delay, forkAff, launchAff_)
+import Halogen as H
+import Halogen.Aff (awaitBody)
+import Halogen.HTML as HH
+import Halogen.HTML.CSS as CSS
+import Halogen.VDom.Driver (runUI)
+
+-- | Here we get the 'return value' of the monadic `runUI` function,
+-- | a record with the type, `HalogenIO`.
+-- | We use this record to communicate with our component.
+main :: Effect Unit
+main =
+    launchAff_ do
+      body <- awaitBody
+      let totalMilliseconds = 5000.0
+      let increment = 100.0
+      io <- runUI topLevelComponent totalMilliseconds body
+
+      forkAff do
+        countdown io totalMilliseconds increment
+  where
+    countdown io secondsRemaining increment = do
+      delay (Milliseconds increment)
+      let remaining = secondsRemaining - increment
+      if remaining <= 0.0
+        then do
+          io.dispose
+        else do
+          void $ io.query $ H.tell $ SetCountDownState remaining
+          countdown io remaining increment
+
+-- Below is a component that doesn't render anything interesting
+-- but can respond to queries.
+
+type State = Number
+type Input = Number
+type Message = Void
+type Action = Void
+data Query a = SetCountDownState State a
+type NoChildSlots = ()
+type MonadType = Aff
+
+topLevelComponent :: H.Component HH.HTML Query Input Message MonadType
+topLevelComponent =
+    H.mkComponent
+      { initialState: identity
+      , render
+      , eval: H.mkEval $ H.defaultEval { handleQuery = handleQuery }
+      }
+  where
+    render :: State -> H.ComponentHTML Action NoChildSlots Aff
+    render remainingMilliseconds =
+        HH.div
+          [ CSS.style do
+            padding (em 2.0) (em 2.0) (em 2.0) (em 2.0)
+            backgroundColor lightgreen
+          ]
+          [ HH.div_
+            [ HH.text $ "This component will disappear in " <>
+                         remaining <> " seconds."
+            ]
+          ]
+      where
+        remaining = show $ remainingMilliseconds / 1000.0
+
+    handleQuery :: forall a. Query a
+                -> H.HalogenM State Action NoChildSlots Message Aff (Maybe a)
+    handleQuery = case _ of
+      SetCountDownState newState next -> do
+        put newState
+        pure $ Just next
