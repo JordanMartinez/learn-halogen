7b17d73c0165c65f318ad597ec040c8564b35afa
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-05-16 19:10:50 -0700

    Update ReaderT example to use AppM (actual pattern), not ReaderT

--- src/08-Going-Deeper/02-A-Different-Monad--ReaderT.purs
+++ src/08-Going-Deeper/02-A-Different-Monad--ReaderT.purs
@@ -3,11 +3,14 @@ module GoingDeeper.ADifferentMonad.ReaderT where
 import Prelude
 
 import CSS (backgroundColor, em, lightgreen, padding)
-import Control.Monad.Reader (runReaderT)
+import Control.Monad.Reader (class MonadAsk, ask, asks, runReaderT)
+import Control.Monad.Reader.Trans (ReaderT)
+import Control.Monad.Trans.Class as MonadTrans
 import Data.Const (Const)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect)
 import Halogen (liftEffect)
 import Halogen as H
@@ -16,14 +19,35 @@ import Halogen.HTML as HH
 import Halogen.HTML.CSS as CSS
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
+import Type.Equality (class TypeEquals)
+import Type.Equality as TypeEquals
 import Web.HTML (window)
 import Web.HTML.Window (alert)
+
+type Env = { name :: String }
+newtype AppM a = AppM (ReaderT Env Aff a)
+
+derive newtype instance functorAppM :: Functor AppM
+derive newtype instance applyAppM :: Apply AppM
+derive newtype instance applicativeAppM :: Applicative AppM
+derive newtype instance bindAppM :: Bind AppM
+derive newtype instance monadAppM :: Monad AppM
+derive newtype instance monadEffectAppM :: MonadEffect AppM
+derive newtype instance monadAffAppM :: MonadAff AppM
+
+-- workaround for allowing type synonyms inside type class instances
+instance monadAsk :: (TypeEquals e Env) => MonadAsk e AppM where
+  ask = AppM (asks $ TypeEquals.from)
+
+runAppM :: Env -> AppM ~> Aff
+runAppM env (AppM program) = runReaderT program env
 
 main :: Effect Unit
 main =
     launchAff_ do
       body <- awaitBody
-      let topLevelComponent = H.hoist (\app -> runReaderT app unit) specialMonadComponent
+      let env = { name: "PureScript" }
+      let topLevelComponent = H.hoist (\app -> runAppM env app) specialMonadComponent
       runUI topLevelComponent unit body
 
 -- Below is a button component that, when clicked,
@@ -38,6 +62,7 @@ type NoChildSlots = ()
 
 specialMonadComponent :: forall m
                        . MonadEffect m
+                      => MonadAsk Env m
                       => H.Component HH.HTML Query Input Message m
 specialMonadComponent =
     H.mkComponent
@@ -62,5 +87,7 @@ specialMonadComponent =
     handleAction :: Action -> H.HalogenM State Action NoChildSlots Message m Unit
     handleAction = case _ of
       PrintAlert -> do
+        rec <- MonadTrans.lift $ ask
         liftEffect do
-          window >>= alert "We're using a non-Aff monad here"
+          window >>= alert ("We're using a non-Aff monad here. Name: " <>
+                            rec.name)
