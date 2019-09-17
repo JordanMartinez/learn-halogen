fb3f911062e449969ec3e2bd61f2842e3ddd12c2
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-09-16 18:33:01 -0700

    Inline scaffolded code into files: Childlike components folder

--- src/03-Parent-Child-Relationships/01-Childlike-Components/04-Message-Only.purs
+++ src/03-Parent-Child-Relationships/01-Childlike-Components/04-Message-Only.purs
@@ -1,19 +1,24 @@
 module ParentChildRelationships.ChildlikeComponents.MessageOnly where
 
-
 import Prelude
 
 import Control.Monad.State (get, modify_)
 import Data.Maybe (Maybe(..))
-import Effect (Effect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
-import Scaffolding.DynamicRenderer.StateAndEval (StateAndActionRenderer)
-import Scaffolding.ParentChildRenderer.ChildlikeComponents.MessageOnlyRenderer (StateActionMessageComponent, HandleAction_StateStringMessage, runStateActionMessageComponent)
 
-main :: Effect Unit
-main = runStateActionMessageComponent textAndButtonComponent
+-- Imports for scaffolded code
+import CSS (em, marginTop)
+import Data.Array ((:))
+import Data.Const (Const)
+import Data.Symbol (SProxy(..))
+import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Halogen (ComponentHTML)
+import Halogen.Aff (awaitBody)
+import Halogen.HTML.CSS as CSS
+import Halogen.VDom.Driver (runUI)
 
 type State = Int
 
@@ -69,3 +74,99 @@ handleAction = case _ of
     H.raise $ show currentState
   NotifyParentTextMessage message -> do
     H.raise $ message
+
+main :: Effect Unit
+main = runStateActionMessageComponent textAndButtonComponent
+
+-- Scaffolded Code --
+
+-- | Renders HTML that can respond to events by translating them
+-- | into a value of the `action` that one uses to handle the event.
+type DynamicHtml action = ComponentHTML action () Aff
+
+-- | A function that uses the `state` type's value to render HTML
+-- | with simple event-handling via the `action` type.
+type StateAndActionRenderer state action = (state -> DynamicHtml action)
+
+-- | When an `action` type's value is received, this function
+-- | determines how to update the component (e.g. state updates) and
+-- | can raise a `String` message to its parent component.
+type HandleAction_StateStringMessage state action =
+  (action -> H.HalogenM state action () String Aff Unit)
+
+-- | Combines all the code we need to define a simple componenet that supports
+-- | state and simple event handlinGg
+type StateActionMessageComponent state action =
+  { initialState :: state
+  , render :: StateAndActionRenderer state action
+  , handleAction :: HandleAction_StateStringMessage state action
+  }
+
+-- | Runs a component that converts the value of the `input` type provided
+-- | by the parent (an `Int`) to a value of the `state` type as the
+-- | child's initial state value, which is used to render dynamic HTML
+-- | with event handling via the `action` type.
+runStateActionMessageComponent :: forall state action.
+                               StateActionMessageComponent state action
+                            -> Effect Unit
+runStateActionMessageComponent childSpec = do
+  launchAff_ do
+    body <- awaitBody
+    runUI (parentComponent $ stateActionMessageComponent childSpec) unit body
+
+type ChildComponentWithStringMessage = H.Component HH.HTML (Const Unit) Unit String Aff
+
+-- | Wraps Halogen types cleanly, so that one gets very clear compiler errors
+stateActionMessageComponent :: forall state action.
+                               StateActionMessageComponent state action
+                            -> ChildComponentWithStringMessage
+stateActionMessageComponent spec =
+  H.mkComponent
+    { initialState: \_ -> spec.initialState
+    , render: spec.render
+    , eval: H.mkEval $ H.defaultEval { handleAction = spec.handleAction }
+    }
+
+data ParentAction = AddMessage String
+type ParentState = Array String
+type ParentQuery = Const Unit
+type ParentComponent = H.Component HH.HTML ParentQuery Unit Void Aff
+
+_child :: SProxy "child"
+_child = SProxy
+
+type NoQuery_StringMessage {- index -}
+  = H.Slot (Const Unit) String {- index -}
+type ChildSlots = ( child :: NoQuery_StringMessage Unit )
+
+parentComponent :: ChildComponentWithStringMessage -> ParentComponent
+parentComponent childComp =
+    H.mkComponent
+      { initialState: const []
+      , render: parentHtml
+      , eval: H.mkEval $ H.defaultEval { handleAction = handleParentAction }
+      }
+  where
+    parentHtml :: ParentState -> H.ComponentHTML ParentAction ChildSlots Aff
+    parentHtml logArray =
+      HH.div_
+        [ HH.div_
+          [ HH.text "Use your child component's html to send messages to \
+                    \the parent. It will render the message below them all"
+          ]
+        , HH.slot _child unit childComp unit (\msg -> Just $ AddMessage msg)
+        , HH.div
+          [ CSS.style do
+            marginTop $ em 3.0
+          ]
+          (logArray <#> \log ->
+            HH.div_ [ HH.text $ "Message received: " <> log]
+          )
+        ]
+
+    handleParentAction
+      :: ParentAction
+      -> H.HalogenM ParentState ParentAction ChildSlots Void Aff Unit
+    handleParentAction (AddMessage msg) = do
+      -- add the message to the front of the array
+      modify_ (\array -> msg : array)
